86eb8d4aa8912db4bc2e4cb9d5cf3e1992f5e514
NickoManu <manuel.nicoletta@gmail.com>
2020-11-29 11:12:43 +0100

    2 files modified

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,7 +1,7 @@
 import { Component, Input, OnInit } from "@angular/core";
 
 @Component({
-  selector: "app-user",
+  selector: "tr[app-user]",
   templateUrl: "./user.component.html",
   styleUrls: ["./user.component.css"]
 })
